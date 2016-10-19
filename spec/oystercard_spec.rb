68c05f1658e280b39e06d5c3bbc6b38c86e6ced0
Royston <rlqhenson@hotmail.com>
2016-10-19 18:52:54 +0100

    add 0 balance on card at initialisation

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require "./lib/oystercard"
 
 describe Oystercard do
+  subject {described_class.new}
+
+  describe "#initialize"
+    it "initialises card with balance of '0'" do
+      expect(subject.balance).to eq(0)
+    end
 end
